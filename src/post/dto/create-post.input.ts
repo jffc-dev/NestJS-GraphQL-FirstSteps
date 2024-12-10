178bdd65e8a3e20587532e3a79c4061231045ccc
Javier Flores <jffc.dev@gmail.com>
2024-12-10 14:54:19 -0500

    feat: validations added

--- src/post/dto/create-post.input.ts
+++ src/post/dto/create-post.input.ts
@@ -1,10 +1,21 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 @InputType()
 export class CreatePostInput {
+  @MaxLength(100, {
+    message: 'The title is too long',
+  })
+  @MinLength(3, {
+    message: 'The title is too short',
+  })
+  @IsNotEmpty({
+    message: 'The title is required',
+  })
   @Field()
   title: string;
 
-  @Field()
-  content: string;
+  @MaxLength(400)
+  @Field({ nullable: true })
+  content?: string;
 }
